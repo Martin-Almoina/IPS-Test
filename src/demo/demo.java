b6dbc64c68715c05b3495672dd3e9c18732a4966
Martin-Almoina <uo300717@uniovi.es>
2025-09-27 18:42:36 +0200

    aaaaaaaa

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -1,9 +1,9 @@
 package demo;
 
 public class demo {
-	//Branch 2
+
 	public static void main(String[] args) {
 	for (int i = 5; i >0; i--)
-		System.out.println(i);
+		System.out.println("World hello");
 	}
 }
