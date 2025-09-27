2fe2a4609b6de78d1004cee6d4115d0eb411b931
Martin-Almoina <uo300717@uniovi.es>
2025-09-27 18:52:45 +0200

    ghj

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -3,7 +3,7 @@ package demo;
 public class demo {
 	//Branch 1
 	public static void main(String[] args) {
-	for (int i = 0; i < 15; i++)
-		System.out.println("world hello");
+	for (int i = 0; i < 150; i++)
+		System.out.println(i);
 	}
 }
