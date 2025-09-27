afd7a3e06235cf342ab869dccdd844786b0f0264
Martin-Almoina <uo300717@uniovi.es>
2025-09-27 18:52:05 +0200

    two lines

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -3,7 +3,9 @@ package demo;
 public class demo {
 
 	public static void main(String[] args) {
-	for (int i = 5; i >0; i--)
-		System.out.println("World hello");
+	for (int i = 50; i >0; i--) {
+		System.out.println(" hello");
+		System.out.println(" world");
+	}
 	}
 }
